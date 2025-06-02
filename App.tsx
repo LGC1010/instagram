c78f58537a4babd07ce74f7fda2ceb8a4ad2bed0
lim <dntksql1010@naver.com>
2025-06-02 16:14:32 +0900

    feat: navigate 수정

--- App.tsx
+++ App.tsx
@@ -1,23 +1,22 @@
-import { FlatList } from 'react-native';
-import PostCard from './src/components/PostCard';
-import './global.css';
-import { GestureHandlerRootView, ScrollView } from 'react-native-gesture-handler';
-import StoryList from './src/components/StoryList';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabNavigator from './src/navigation/BottomTabNavigator';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import PostDetailScreen from './src/screens/PostDetailScreen';
+import { RootStackParamList } from './src/types/navigation.';
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  const dummyData = Array.from({ length: 10 }, (_, i) => i); // 더미 데이터
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
-          <NavigationContainer>
-            <BottomTabNavigator />
-          </NavigationContainer>
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+        <RootStack.Screen name='MainTabs' component={BottomTabNavigator} />
+        <RootStack.Screen
+          name='PostDetailScreen'
+          component={PostDetailScreen}
+          options={{ animation: 'slide_from_right' }}
+        />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
